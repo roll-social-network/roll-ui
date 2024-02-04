bc912cb4597d5ac737707b67f23b998c3b7c4321
Douglas Paz <eu@douglaspaz.com>
2024-02-04 00:08:37 -0300

    feat: add fontawesome solid

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue/dist/vue.esm-bundler'
 import './sass/main.scss'
 import './fontawesome-free-6.5.1-web/scss/fontawesome.scss'
 import './fontawesome-free-6.5.1-web/scss/regular.scss'
+import './fontawesome-free-6.5.1-web/scss/solid.scss'
 
 const timelineApp = createApp({
 })

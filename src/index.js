04ca940da06a3f022911fc833eb05e9abd7e25ea
Douglas Paz <eu@douglaspaz.com>
2024-02-03 22:10:57 -0300

    feat: add fontawesome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue/dist/vue.esm-bundler';
-import './sass/main.scss';
+import { createApp } from 'vue/dist/vue.esm-bundler'
+import './sass/main.scss'
+import './fontawesome-free-6.5.1-web/scss/fontawesome.scss'
+import './fontawesome-free-6.5.1-web/scss/regular.scss'
 
-const timelineApp = createApp({});
+const timelineApp = createApp({
+})
 
-timelineApp.mount('#timeline');
+timelineApp.mount('#timeline')

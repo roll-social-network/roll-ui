bdb54215845922cf061e439c7eaa6ca49f459996
Douglas Paz <eu@douglaspaz.com>
2024-05-05 17:42:56 -0300

    feat: init footer

---
+++ lib/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <p>build with <a href="https://github.com/roll-social-network" target="_blank">roll social network</a></p>
+    </footer>
+  )
+}
+export default Footer

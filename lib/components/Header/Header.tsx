14cb742388cd6651b480f67436a980d0f6151186
Douglas Paz <eu@douglaspaz.com>
2024-06-02 16:14:45 -0300

    feat: init sass style

--- lib/components/Header/Header.tsx
+++ lib/components/Header/Header.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars'
+import { faUser } from '@fortawesome/free-solid-svg-icons/faUser'
 
 interface HeaderProps {
   title?: String
@@ -7,7 +10,13 @@ interface HeaderProps {
 export const Header = ({ title = 'roll' }: HeaderProps) => {
   return (
     <header>
+      <ul>
+        <li><a href="#"><FontAwesomeIcon icon={faBars} /></a></li>
+      </ul>
       <h1>{title}</h1>
+      <ul>
+        <li><a href="#"><FontAwesomeIcon icon={faUser} /></a></li>
+      </ul>
     </header>
   )
 }
